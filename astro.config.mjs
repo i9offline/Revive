8cf75a14882867051b018ac083ad823dd2979123
Reynaud Olivier <91701330+i9offline@users.noreply.github.com>
2025-12-09 22:38:27 +0100

    Fix: Add base path to astro config for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://i9offline.github.io',
+  base: '/Revive',
   vite: {
     plugins: [tailwindcss()]
   },
