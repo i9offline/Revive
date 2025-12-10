d7b82cd1149563e1e472f74e57a5a8885b8f4f46
Reynaud Olivier <91701330+i9offline@users.noreply.github.com>
2025-12-10 09:18:57 +0100

    Fix: Migrate images to src/assets for reliable path resolution

--- src/components/FloatingBottle.jsx
+++ src/components/FloatingBottle.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { getAssetPath } from '../utils/url.js';
+import bottleImage from '../assets/images/revive-bottle.png';
 
 
 export default function FloatingBottle() {
   return (
     <div className="relative w-72 h-auto md:w-[400px]">
       <img
-        src={getAssetPath("images/revive-bottle.png")}
+        src={bottleImage.src}
         alt="Revive Bottle"
         className="w-full h-auto drop-shadow-2xl"
       />
