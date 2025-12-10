acf32de52706f6e1410bc45b86004a483af5547e
Reynaud Olivier <91701330+i9offline@users.noreply.github.com>
2025-12-10 09:23:09 +0100

    Fix: Pass image as prop to React component to resolve build issues

--- src/components/FloatingBottle.jsx
+++ src/components/FloatingBottle.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import bottleImage from '../assets/images/revive-bottle.png';
+import React from 'react';
 
 
-export default function FloatingBottle() {
+export default function FloatingBottle({ imageSrc }) {
   return (
     <div className="relative w-72 h-auto md:w-[400px]">
       <img
-        src={bottleImage.src}
+        src={imageSrc}
         alt="Revive Bottle"
         className="w-full h-auto drop-shadow-2xl"
       />
